2723ea55d352996c32845e78445a8d7857eef69c
ksasakipz <ksasaki@students.pitzer.edu>
2021-07-25 01:01:18 -0700

    Typos

--- cpp/pyramid.cpp
+++ cpp/pyramid.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 int main() {
 	while (false) {
-		std::cout <<"Goodbye World\n";
+		std::cout <<"Goodbye, World\n";
 	}
-	std::cout << "hello World\n";
+	std::cout << "Hello, World\n";
 }
