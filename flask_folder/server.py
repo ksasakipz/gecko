ec27a0fed61716105d8c26505c814cc7e99effad
ksasakipz <ksasaki@students.pitzer.edu>
2021-08-18 06:53:17 -0700

    s

--- flask_folder/server.py
+++ flask_folder/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-PIN = 275-961-552=-1238
+PIN = 275-961-552-1238
+PIN += 96235
+PIN -= 93759
 
 
-Kevin ="""I do solemnly swear..."""
+Kevirsn ="""I do solemnly swear..."""
 line_break=""""""
 Rachael ="""I vow to..."""
 forever = """I do solemnly swear to remain loyal to you until death due us part. I vow to spend the rest of my life learning how to love you best."""
@@ -15,7 +17,7 @@ def index():
 
 @app.route('/my-link/')
 def my_link():
-  return (str(PIN))
+  return (forever + str(PIN))
   return (forever)
 
 if __name__ == '__main__':
